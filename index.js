408a257f6c32b4b42bd38bf9f07d862684a15582
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2022-06-28 14:41:50 +0530

    chore: deploy check

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-	res.send("Hare Krishna To Express");
+	res.send("Hare Krishna To Express Server");
 });
 
 app.listen(process.env.PORT || port, () => {

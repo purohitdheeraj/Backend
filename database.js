29ba673f5721bfa29d57bf67cec51dde3e308231
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2022-06-28 15:49:52 +0530

    Feat: Server Hosted on Heroku ✅

---
+++ database.js
@@ -0,0 +1,20 @@
+const database = {
+	products: [
+		{
+			id: 123,
+			title: "Wooden Venu",
+			description: "bansuri",
+		},
+		{
+			id: 124,
+			title: "Morpakha",
+			description: "mayur feather",
+		},
+		{
+			id: 125,
+			title: "Peet Vastram",
+			description: "Yellow Clothes",
+		},
+	],
+};
+exports.database = database;
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const { database } = require("./database");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hare Krishna To Express Server");
-});
+const takeMeHome = (req, res) => {
+	res.send("Radhe Radhe Home Pages");
+};
+
+const getAllProducts = (req, res) => {
+	const productsData = database.products
+		.map((item) => `<li>${item.title}</li>`)
+		.join("\n");
+
+	res.send(productsData);
+};
+
+app.get("/", takeMeHome);
+app.get("/products", getAllProducts);
 
 app.listen(process.env.PORT || port, () => {
 	console.log("hare krishna server started");
